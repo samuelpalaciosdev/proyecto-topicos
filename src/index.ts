db45f9323eff63fd2ee66f9741a5492403368c75
Samuel Palacios <samuelpl0888@gmail.com>
2024-12-24 06:12:28 -0400

    add: test get chiste

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import "dotenv/config"; // Carga variables locales del .env
+import 'dotenv/config'; // Carga variables locales del .env
 
-import express from "express";
-import connectDb from "./db/connect";
+import express from 'express';
+import connectDb from './db/connect';
 
 const app = express();
 const PORT = process.env.PORT || 3005;
 
-app.get("/", (_req, res) => {
-  res.send("Setup funcionando").status(200);
+app.get('/', (_req, res) => {
+  res.send('Setup funcionando').status(200);
 });
 
 // Inicia el servidor
 const start = async () => {
   try {
-    await connectDb();
+    // await connectDb();
     app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
   } catch (error) {
     console.log(error);
