a2bf886ce32e8dc6e61007b528509d236357e054
Samuel Palacios <samuelpl0888@gmail.com>
2024-12-25 22:15:19 -0400

    add: test del endpoint get chiste dad

--- src/tests/get-chiste.test.ts
+++ src/tests/get-chiste.test.ts
@@ -3,6 +3,7 @@ import app from "../index";
 import mongoose from "mongoose";
 mongoose.set("strictQuery", false);
 
+// Ref: ver si puedo hacer esto con lo que tengo en db/
 const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_PORT, MONGO_DB, MONGO_HOSTNAME } =
   process.env;
 
@@ -17,7 +18,7 @@ describe("GET api/chistes?fuente=$parametro", () => {
     await mongoose.connection.close();
   });
 
-  it("GET api/chistes?fuente=chuck Debería traer un chiste de la api de chuck", async () => {
+  it("GET api/chistes?fuente=chuck Debería traer un chiste de la api de chuck jokes", async () => {
     const response = await request(app).get("/api/chistes?fuente=chuck");
 
     expect(response.status).toBe(200); // Espero que el estatus sea ok
@@ -25,5 +26,15 @@ describe("GET api/chistes?fuente=$parametro", () => {
     expect(response.body).toHaveProperty("id");
     expect(response.body).toHaveProperty("value");
     expect(response.body).toHaveProperty("url");
+  });
+
+  it("GET api/chistes?fuente=dad Debería traer un chiste de la api de dad jokes", async () => {
+    const response = await request(app).get("/api/chistes?fuente=dad");
+
+    expect(response.status).toBe(200); // Espero que el estatus sea ok
+    // Expect propiedades
+    expect(response.body).toHaveProperty("id");
+    expect(response.body).toHaveProperty("joke");
+    expect(response.body).toHaveProperty("status");
   });
 });
