65654599da4efa26251eb4cac8334c8531d5fe1c
Samuel Palacios <samuelpl0888@gmail.com>
2024-12-27 19:30:00 -0400

    add: rate limiter on post request

--- src/routes/chiste-routes.ts
+++ src/routes/chiste-routes.ts
@@ -1,17 +1,29 @@
-
 import { Router } from "express";
 import {
   getChisteByFuente,
   getChistesByPuntaje,
 } from "../controllers/chiste-controller";
-import { createChiste, getChisteById, putChiste } from "../controllers/chiste-controller";
-import { validation } from '../middleware/middleware';
-import { chisteSchema } from '../validations/chiste-schema';
+import {
+  createChiste,
+  getChisteById,
+  putChiste,
+} from "../controllers/chiste-controller";
+import { validation } from "../middleware/middleware";
+import { chisteSchema } from "../validations/chiste-schema";
+import { rateLimit } from "express-rate-limit";
 
 const router = Router();
 
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 min
+  limit: 3,
+  message: {
+    msg: "Muchas solicitudes desde esta IP. Por favor, espera 5 minutos e inténtalo de nuevo.",
+  },
+});
+
 router.get("/", getChistesByPuntaje);
-router.post("/", createChiste);
+router.post("/", limiter, createChiste); // Aplicando rate limiter al post
 router.get("/:id", getChisteById);
 router.put("/:id", validation(chisteSchema), putChiste);
 router.get("/fuente/:fuente", getChisteByFuente);
