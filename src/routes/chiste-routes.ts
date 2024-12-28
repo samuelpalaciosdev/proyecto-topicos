11b8381918f316f9ff3a76f5a1b9c33faabf5747
danirssx <rossdaniel5018@gmail.com>
2024-12-27 22:17:26 -0400

    Fixed

--- src/routes/chiste-routes.ts
+++ src/routes/chiste-routes.ts
@@ -2,8 +2,6 @@ import { Router } from "express";
 import {
   deleteChisteById,
   getChisteByFuente,
-  getChistes,
-  getChistesByPuntaje,
 } from "../controllers/chiste-controller";
 import {
   createChiste,
@@ -14,14 +12,9 @@ import { queryGetHandler, validation } from "../middleware/middleware";
 import { chisteSchema } from "../validations/chiste-schema";
 import { rateLimit } from "express-rate-limit";
 
-
 const router = Router();
 
-// Rama principal
-router.get("/", queryGetHandler);
-
 // Requerimientos
-router.get("/fuente/:fuente", getChisteByFuente);
 router.post("/", createChiste);
 const limiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 min
@@ -30,9 +23,12 @@ const limiter = rateLimit({
     msg: "Muchas solicitudes desde esta IP. Por favor, espera 5 minutos e inténtalo de nuevo.",
   },
 });
+
+router.get("/", queryGetHandler);
 router.post("/", limiter, createChiste); // Aplicando rate limiter al post
 router.get("/:id", getChisteById);
 router.put("/:id", validation(chisteSchema), putChiste);
 router.delete("/:id", deleteChisteById);
+router.get("/fuente/:fuente", getChisteByFuente);
 
 export default router;
